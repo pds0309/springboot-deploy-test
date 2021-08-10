909fdb6ee5ffb9e6e21ac56e71d93276ebc0ac09
pds0309 <ehd0309@gmail.com>
2021-08-10 10:42:54 +0900

    Add Controller, profile for nginx

--- src/main/java/com/test/testcicd/ProfileController.java
+++ src/main/java/com/test/testcicd/ProfileController.java
@@ -1,19 +1,25 @@
 package com.test.testcicd;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
     private final Environment env;
+
+    @Value("${abc}")
+    private String prodVal;
+
+    @GetMapping("/abc")
+    public String getProdProfile(){
+        return prodVal;
+    }
 
     @GetMapping("/profile")
     public String getProfile () {
--- src/main/java/com/test/testcicd/TestcicdApplication.java
+++ src/main/java/com/test/testcicd/TestcicdApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestcicdApplication {
 
+
     public static void main(String[] args) {
+        if(System.getProperty("spring.profiles.default")==null){
+            System.setProperty("spring.profiles.default" , "test");
+        }
         SpringApplication.run(TestcicdApplication.class, args);
     }
 
